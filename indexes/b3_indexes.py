6ecf2cc2ace957703ed3385342dd695145b10f53
Gaston Guillaux <gaston.guillaux@gmail.com>
2018-09-10 23:22:21 -0300

    new function added

--- indexes/b3_indexes.py
+++ indexes/b3_indexes.py
@@ -4,7 +4,7 @@ Created on Wed Sep 10 22:00:13 2018
 
 @author: Gaston Guillaux
 """
-
+#export to csv ibovespa composition
 def get_ibov():
 	from datetime import datetime
 	import pandas as pd
@@ -19,7 +19,7 @@ def get_ibov():
 	ibov_df.to_csv(csv_file, index=False)
 
 
-
+#get url of all b3 indexes
 def get_b3_indexes_urls():
     from bs4 import BeautifulSoup as bs
     import requests
@@ -37,3 +37,20 @@ def get_b3_indexes_urls():
         indices[name] = {}
         indices[name]['url'] = link
     return indices
+
+def get_b3_indexes_carteiras():
+    from bs4 import BeautifulSoup as bs    
+    import requests
+    indexes = get_b3_indexes_urls()
+    parent_url = url = 'http://www.bmfbovespa.com.br/pt_br/produtos/indices/'
+    
+    for index in indexes:
+        url = indexes[index]['url']
+        pagina = requests.get(url)
+        soup = bs(pagina.text, 'html.parser')
+        try:
+            href = soup.select('a[href*="composicao"]')[0]['href'].replace('./', '')
+            indexes[index]['carteira'] =  href
+        except:
+            indexes[index]['carteira'] = '*** index without composition ***'
+    return indexes        
